8f53de050e598ca4491942428f55f05e8a0c1f92
aquadolphin19 <77813989+aquadolphin19@users.noreply.github.com>
2021-01-21 18:00:07 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function calculate() {
+var area= Number(document.getElementById("input").value)*
+Number(document.getElementById("input").value);
+document.getElementById ("lblaream").innerHTML= area;
+var perimeter= 
+    4*
+Number(document.getElementById("input").value);
+document.getElementById ("lblperimeterm").innerHTML= perimeter;
+}
